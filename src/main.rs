68d1053e62cd37451304831e2b33d5b0aeb6f365
Albin <albin@malmo.skaggbyran.se>
2025-12-05 13:01:24 +0100

    Working on algorithm for collating addresses with locations and locations with closest relevant info.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,22 @@
-use iced::widget::{button, column, combo_box, container, row, scrollable, text_input};
+use iced::widget::{button, column, container, row, scrollable, text_input};
 use iced::Element;
+use rusty_math::knn::{KNNeighborsClassifier, KNNeighborsRegressor};
+use serde::Serialize;
+use std::fs::File;
+use std::io::Write;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct Adress {
+#[derive(Default, Serialize)]
+struct AdressInit {
+    gata: String,
+    gatunummer: String,
+    postnummer: String,
+}
+#[derive(Default, Serialize)]
+struct AdressExit {
     gata: String,
     gatunummer: String,
     postnummer: String,
     aktiv: bool,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum Lagring {
-    adress(Adress),
-}
-
-impl std::fmt::Display for Lagring {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Lagring::adress(adress) => "test",
-            }
-        )
-    }
 }
 
 #[derive(Debug, Clone)]
@@ -32,77 +25,164 @@ enum Message {
     GatunummerChanged(String),
     PostnummerChanged(String),
     AddAddressButtonPressed,
-    Selected(Adress),
-    OptionHovered(Adress),
-    Closed,
 }
 
 pub fn main() -> iced::Result {
-    iced::run("amp", Adress::update, Adress::view)
+    knnc_eu();
+    knnc_min();
+    knnc_cos();
+    knnc_man();
+    knnr_eu();
+    knnr_min();
+    knnr_cos();
+    knnr_man();
+    iced::run("amp", update, view)
 }
 
-impl Adress {
-    fn new() -> Self {
-        Self {
-            gata: "".to_string(),
-            gatunummer: "".to_string(),
-            postnummer: "".to_string(),
-            aktiv: false,
-        }
-    }
-    fn view(state: &Adress) -> Element<'_, Message> {
-        column![
-            container(row![
-                text_input("Gata", &state.gata).on_input(Message::GataChanged),
-                text_input("Gatunummer", &state.gatunummer).on_input(Message::GatunummerChanged),
-                text_input("Postnummer", &state.postnummer).on_input(Message::PostnummerChanged),
-                button("+").on_press(Message::AddAddressButtonPressed),
-            ])
-            .padding(10)
-            .style(container::rounded_box),
-            container(scrollable(column![
-                //Parse stored addresses from JSON
-                combo_box(
-                    &state.gata,
-                    "Select your favorite fruit...",
-                    Option::from(state.gata.as_ref()),
-                    Message::Selected(*state),)
-            ]))
-            .padding(10)
-            .style(container::rounded_box)
-        ]
-            .into()
-    }
+fn view(state: &AdressInit) -> Element<'_, Message> {
+    column![
+        container(row![
+            text_input("Gata", &state.gata).on_input(Message::GataChanged),
+            text_input("Gatunummer", &state.gatunummer).on_input(Message::GatunummerChanged),
+            text_input("Postnummer", &state.postnummer).on_input(Message::PostnummerChanged),
+            button("+").on_press(Message::AddAddressButtonPressed),
+        ])
+        .padding(10)
+        .style(container::rounded_box),
+        container(scrollable(column![
+            //Parse stored addresses from JSON
 
-    fn update(state: &mut Adress, message: Message) {
-        match message {
-            Message::GataChanged(gata) => {
-                state.gata = gata;
-            }
-            Message::GatunummerChanged(gatunummer) => {
-                state.gatunummer = gatunummer;
-            }
-            Message::PostnummerChanged(postnummer) => {
-                state.postnummer = postnummer;
-            }
-            Message::AddAddressButtonPressed => {
-                //let _content = state.content.clone(); //Add to JSON list and write
-            }
-            Message::Selected(adress) => {
-                state.gata = adress.gata;
-            }
-            Message::OptionHovered(adress) => {
-                state.gata = adress.gata.to_string();
-            }
-            Message::Closed => {
-                state.gata = state.gata.clone();
-            }
+        ]))
+        .padding(10)
+        .style(container::rounded_box)
+    ]
+        .into()
+}
+
+fn update(state: &mut AdressInit, message: Message) {
+    match message {
+        Message::GataChanged(gata) => {
+            state.gata = gata;
+        }
+        Message::GatunummerChanged(gatunummer) => {
+            state.gatunummer = gatunummer;
+        }
+        Message::PostnummerChanged(postnummer) => {
+            state.postnummer = postnummer;
+        }
+        Message::AddAddressButtonPressed => {
+            write_json(state).unwrap(); //Add to JSON list and write //Remove unwrap
         }
     }
 }
 
-impl Default for Adress {
-    fn default() -> Self {
-        Adress::new()
-    }
+fn write_json(data: &mut AdressInit) -> std::io::Result<()> {
+    let structure = AdressExit {
+        gata: data.gata.clone(),
+        gatunummer: data.gatunummer.clone(),
+        postnummer: data.postnummer.clone(),
+        aktiv: true,
+    };
+    let json_data = serde_json::to_string_pretty(&structure)?;
+    let mut file = File::create("adresser.json")?;
+    file.write_all(json_data.as_bytes())?;
+    Ok(())
+}
+
+fn knnc_eu() { //-> Vec<i32>
+    let mut knn = KNNeighborsClassifier::new(2, "euclidean".to_string(), None);
+    let x_train = vec![vec![55.567676, 13.011854], vec![55.567637, 13.012091]];
+    let y_train = vec![55.5568509, 12.9182615];
+    knn.fit(&x_train, &y_train);
+    let x = vec![55.5568509, 12.9182615];
+    let y = knn.knearest_neighbors(&x);
+    let params = knn.get_params();
+    println!("KNNC euclidean");
+    println!("{:#?}", y);
+    println!("{:#?}", params);
+}
+fn knnc_min() { //-> Vec<i32>
+    let mut knn = KNNeighborsClassifier::new(2, "minkowski".to_string(), Some(50));
+    let x_train = vec![vec![55.567676, 13.011854], vec![55.567637, 13.012091]];
+    let y_train = vec![55.5568509, 12.9182615];
+    knn.fit(&x_train, &y_train);
+    let x = vec![55.5568509, 12.9182615];
+    let y = knn.knearest_neighbors(&x);
+    let params = knn.get_params();
+    println!("KNNC minkowski");
+    println!("{:#?}", y);
+    println!("{:#?}", params);
+}
+fn knnc_cos() { //-> Vec<i32>
+    let mut knn = KNNeighborsClassifier::new(2, "cosine".to_string(), None);
+    let x_train = vec![vec![55.567676, 13.011854], vec![55.567637, 13.012091]];
+    let y_train = vec![55.5568509, 12.9182615];
+    knn.fit(&x_train, &y_train);
+    let x = vec![55.5568509, 12.9182615];
+    let y = knn.knearest_neighbors(&x);
+    let params = knn.get_params();
+    println!("KNNC cosine");
+    println!("{:#?}", y);
+    println!("{:#?}", params);
+}
+fn knnc_man() { //-> Vec<i32>
+    let mut knn = KNNeighborsClassifier::new(2, "manhattan".to_string(), None);
+    let x_train = vec![vec![55.567676, 13.011854], vec![55.567637, 13.012091]];
+    let y_train = vec![55.5568509, 12.9182615];
+    knn.fit(&x_train, &y_train);
+    let x = vec![55.5568509, 12.9182615];
+    let y = knn.knearest_neighbors(&x);
+    let params = knn.get_params();
+    println!("KNNC manhattan");
+    println!("{:#?}", y);
+    println!("{:#?}", params);
+}
+
+fn knnr_eu() { //-> Vec<i32>
+    let mut knn = KNNeighborsRegressor::new(2, "euclidean".to_string(), None);
+    let x_train = vec![vec![55.567676, 13.011854], vec![55.567637, 13.012091]];
+    let y_train = vec![55.5568509, 12.9182615];
+    knn.fit(&x_train, &y_train);
+    let x = vec![55.5568509, 12.9182615];
+    let y = knn.knearest_neighbors(&x);
+    let params = knn.get_params();
+    println!("KNNR euclidean");
+    println!("{:#?}", y);
+    println!("{:#?}", params);
+}
+fn knnr_min() { //-> Vec<i32>
+    let mut knn = KNNeighborsRegressor::new(2, "minkowski".to_string(), Some(50));
+    let x_train = vec![vec![55.567676, 13.011854], vec![55.567637, 13.012091]];
+    let y_train = vec![55.5568509, 12.9182615];
+    knn.fit(&x_train, &y_train);
+    let x = vec![55.5568509, 12.9182615];
+    let y = knn.knearest_neighbors(&x);
+    let params = knn.get_params();
+    println!("KNNR minkowski");
+    println!("{:#?}", y);
+    println!("{:#?}", params);
+}
+fn knnr_cos() { //-> Vec<i32>
+    let mut knn = KNNeighborsRegressor::new(2, "cosine".to_string(), None);
+    let x_train = vec![vec![55.567676, 13.011854], vec![55.567637, 13.012091]];
+    let y_train = vec![55.5568509, 12.9182615];
+    knn.fit(&x_train, &y_train);
+    let x = vec![55.5568509, 12.9182615];
+    let y = knn.knearest_neighbors(&x);
+    let params = knn.get_params();
+    println!("KNNR cosine");
+    println!("{:#?}", y);
+    println!("{:#?}", params);
+}
+fn knnr_man() { //-> Vec<i32>
+    let mut knn = KNNeighborsRegressor::new(2, "manhattan".to_string(), None);
+    let x_train = vec![vec![55.567676, 13.011854], vec![55.567637, 13.012091]];
+    let y_train = vec![55.5568509, 12.9182615];
+    knn.fit(&x_train, &y_train);
+    let x = vec![55.5568509, 12.9182615];
+    let y = knn.knearest_neighbors(&x);
+    let params = knn.get_params();
+    println!("KNNR manhattan");
+    println!("{:#?}", y);
+    println!("{:#?}", params);
 }
